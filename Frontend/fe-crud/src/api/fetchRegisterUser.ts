d5874570d6029c39a0494774705ae2cec78af912
Yuri <rodriguesyuri769@gmail.com>
2023-08-17 15:16:54 -0300

    feat: adicionando o loading pro componente

---
+++ Frontend/fe-crud/src/api/fetchRegisterUser.ts
@@ -0,0 +1,23 @@
+import { RegisterFormProps } from "../types/registerFormProps";
+
+export const fetchRegisterUser = async (data: RegisterFormProps) => {
+  try {
+    const res = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password,
+        passwordConfirmation: data.confirmPassword,
+      }),
+    });
+    const result = await res.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ Frontend/fe-crud/src/components/Loading/Loading.tsx
@@ -0,0 +1,10 @@
+import { BiLoaderAlt } from "react-icons/bi";
+import './loading.sass'
+
+const Loading = () => {
+  return (
+    <p className="loading-p"><BiLoaderAlt /></p>
+  )
+};
+
+export default Loading;
