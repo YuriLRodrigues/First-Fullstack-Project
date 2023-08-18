e8c9dd26f1c8e5ccf4bfddafbb6bc930c7891045
Yuri <rodriguesyuri769@gmail.com>
2023-08-18 10:58:30 -0300

    Atualizando a rota get

--- Backend/src/express.js
+++ Backend/src/express.js
@@ -2,48 +2,66 @@ const express = require("express");
 const UserModel = require("./models/user.model");
 const router = express.Router();
 
-router.get("/users", async (req, res) => {
+router.post("/usersregister", async (req, res) => {
   try {
-    const users = await UserModel.find();
-    res.status(200).json(users);
+    if (
+      req.body.password === process.env.PASSWORD &&
+      req.body.user === process.env.USER
+    ) {
+      const users = await UserModel.find();
+      res.status(200).json(users);
+    } else {
+      res.status(401).send('Não autorizado')
+    }
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
-router.post('/users', async (req, res)=> {
+router.post("/users", async (req, res) => {
   try {
-    const {firstName, email} = req.body
-    const allUsers = await UserModel.find()
-    const verifyUser = allUsers.filter((user)=> user.firstName === firstName || user.email === email)
-    if (verifyUser.length === 0){
-      const user = await UserModel.create(req.body)
-      return res.status(201).json(user)
+    const { firstName, email } = req.body;
+    const allUsers = await UserModel.find();
+    const verifyUser = allUsers.filter(
+      (user) => user.firstName === firstName || user.email === email
+    );
+    if (verifyUser.length === 0) {
+      const user = await UserModel.create(req.body);
+      return res.status(201).json(user);
     }
-    res.status(500).send(`O usuário ${firstName} ou o e-mail ${email} já existe em nosso sistema`)
+    res
+      .status(500)
+      .send(
+        `O usuário ${firstName} ou o e-mail ${email} já existe em nosso sistema`
+      );
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
-})
+});
 
-router.delete('/users', async (req, res) => {
+router.delete("/users", async (req, res) => {
   try {
-    const {email, password, firstName} = req.body
-    const user = await UserModel.findOneAndDelete({email, password, firstName}, {new: true})
-    res.status(201).json(user)
+    const { email, password, firstName } = req.body;
+    const user = await UserModel.findOneAndDelete(
+      { email, password, firstName },
+      { new: true }
+    );
+    res.status(201).json(user);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
-})
+});
 
-router.patch('/users/update', async (req, res)=> {
+router.patch("/users/update", async (req, res) => {
   try {
-    const {email} = req.body
-    const user = await UserModel.findOneAndUpdate({email}, req.body, {new: true})
-    res.status(201).json(user)
+    const { email } = req.body;
+    const user = await UserModel.findOneAndUpdate({ email }, req.body, {
+      new: true,
+    });
+    res.status(201).json(user);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
-})
+});
 
 module.exports = router;
--- Frontend/fe-crud/src/api/getUsers.ts
+++ Frontend/fe-crud/src/api/getUsers.ts
@@ -1,4 +1,13 @@
 export const getUsers = async () => {
-  const res = await fetch(`https://backend-fs-pi.vercel.app/users`)
+  const res = await fetch(`https://backend-fs-pi.vercel.app/usersregister`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: import.meta.env.VITE_PASSWORD,
+      user: import.meta.env.VITE_USER
+    }),
+  })
   return await res.json()
 }
